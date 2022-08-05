8a188af02756f76f8a6df54a8c1b38864257f37a
brhanuh <hanibalbrhanu15@hotmail.com>
2022-08-05 12:16:00 +0100

    basic folder structure

---
+++ mental_health/__init__.py
@@ -0,0 +1,31 @@
+from dotenv import load_dotenv
+from os import environ
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+from .database.db import db
+from .routes.main import main_routes
+
+# load environment
+load_dotenv
+
+database_uri = environ.get('DATABASE_URL')
+
+# Set up the app
+
+app = Flask(__name__)
+app.config.update(
+    SQLALCHEMY_DATABASE_URI=database_uri,
+    SQLALCHEMY_TRACK_MODIFICATIONS=environ.get('SQLALCHEMY_TRACK_MODIFICATIONS')
+)
+
+CORS(app)
+db.app = app
+db.init_app(app)
+
+app.register_blueprint(main_routes)
+
+## Main
+
+if __name__ == "__main__":
+    app.run(debug=True)
