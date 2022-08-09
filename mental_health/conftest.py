865dcd819ec503b24f27706339dce54f1d6a5817
Ayoub_L <ayoublaissaoui.1@gmail.com>
2022-08-09 09:47:35 +0100

    added unittest

---
+++ mental_health/conftest.py
@@ -0,0 +1,17 @@
+# from pip import main
+# import pytest
+# from ..routes.main import 
+# from models.entry import Entry
+
+# @pytest.fixture
+# def api(monkeypatch):
+#     test_entries = [ 
+#     { "date_posted": "08-08-2022", "mood": 5, "energy": 5, "depression": 1, "irritability": 1, "motivation": 1, "stress": 1, "appetatite": 5, "concentration": 5, "diet": "pizza and coke", "enter": "netflix", "social": "facebook, insta, snap" }, 
+#     { "date_posted": "08-08-2022", "mood": 5, "energy": 5, "depression": 1, "irritability": 1, "motivation": 1, "stress": 1, "appetatite": 5, "concentration": 5, "diet": "pizza and coke", "enter": "netflix", "social": "facebook, insta, snap" },
+#     { "date_posted": "08-08-2022", "mood": 5, "energy": 5, "depression": 1, "irritability": 1, "motivation": 1, "stress": 1, "appetatite": 5, "concentration": 5, "diet": "pizza and coke", "enter": "netflix", "social": "facebook, insta, snap" },
+#     { "date_posted": "08-08-2022", "mood": 5, "energy": 5, "depression": 1, "irritability": 1, "motivation": 1, "stress": 1, "appetatite": 5, "concentration": 5, "diet": "pizza and coke", "enter": "netflix", "social": "facebook, insta, snap" }, 
+#     { "date_posted": "08-08-2022", "mood": 1, "energy": 5, "depression": 1, "irritability": 1, "motivation": 1, "stress": 1, "appetatite": 5, "concentration": 5, "diet": "pizza and coke", "enter": "netflix", "social": "facebook, insta, snap" } 
+#     ]
+#     api = main_routes.app.test_client()
+#     return api
+
