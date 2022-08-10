842c82d20db81d84829bf0bfd2f226ed9c7db228
Dakshitha Mak <dakshitha1992@hotmail.com>
2022-08-10 12:12:01 +0100

    view all posts

--- mental_health/routes/recommendation.py
+++ mental_health/routes/recommendation.py
@@ -8,8 +8,28 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 recom_route = Blueprint("recommendation", __name__)
 
+# show all posts by all users
+@recom_route.route("/", methods=["GET"])
+@jwt_required()
+def all__posts():
+    
+    posts = Post.query.all()
+
+    get_data = []
+    for post in posts:
+        get_data.append({
+                "posted_user": post.user.username,
+                "id": post.id, 
+                "type": post.type, 
+                "source": post.source, 
+                "text":post.text,
+                "date_created":post.date_created
+                })
+    return get_data
+
+
 #logged in user is able to create a post
-@recom_route.route("/posts", methods=["POST","GET"])
+@recom_route.route("/post", methods=["POST"])
 @jwt_required()
 def create_post():
     if request.method == 'POST':
@@ -24,8 +44,6 @@ def create_post():
         db.session.commit()
         return 'post created'
 
-    if request.method == 'GET':
-        return 'should redirect to the all posts page'
                 
 
 
